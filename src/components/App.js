2e360dfee09cc68671a83cb1af46f572e8ff89f5
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-01-08 18:17:17 +0100

    Refactor App from a class-based component to a functional component with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './Calculator';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="calc-container">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="calc-container">
+    <Calculator />
+  </div>
+);
+
+export default App;
