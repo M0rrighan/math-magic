530d88c097d3a00a1f0c2302737d366279f80f09
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-01-08 17:41:45 +0100

    Pass function updateCalculator as props to Button component

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,19 +1,31 @@
+/* eslint-disable no-useless-constructor */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
 export default class Buttons extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     const buttonNames = [
-      'AC', '+/-', '%', '/',
-      '7', '8', '9', 'X',
+      'AC', '+/-', '%', '÷',
+      '7', '8', '9', 'x',
       '4', '5', '6', '-',
       '1', '2', '3', '+',
       '0', '.', '=',
     ];
 
+    const { updateCalculator } = this.props;
+
     const buttons = buttonNames.map((btnName) => (
-      <Button key={btnName} buttonName={btnName} />
+      <Button
+        key={btnName}
+        buttonName={btnName}
+        updateCalculator={updateCalculator}
+      />
     ));
     return (
       <div className="buttons">
@@ -22,3 +34,7 @@ export default class Buttons extends Component {
     );
   }
 }
+
+Buttons.propTypes = {
+  updateCalculator: PropTypes.func.isRequired,
+};
