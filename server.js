b320836d33fa7ef82a67e6225353d82a95f39ba5
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-01-18 00:03:02 +0100

    Heroku setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(`${__dirname}/`));
+app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
